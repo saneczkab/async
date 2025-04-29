d1740f2a1ce8a005dc4396b830e1443aae56a7f7
Alexander Iarmoshenko <a2398253@gmail.com>
2025-04-29 18:31:27 +0500

    task 4 with bug

--- static/focus.js
+++ static/focus.js
@@ -7,16 +7,19 @@ const API = {
 
 async function run() {
     try {
-        const orgOgrns = await sendRequest(API.organizationList);
+        const [orgOgrns, requisites, analytics, buh] = await Promise.all([
+            sendRequest(API.organizationList),
+            sendRequest(API.orgReqs),
+            sendRequest(API.analytics),
+            sendRequest(API.buhForms),
+        ]);
+
         const ogrns = orgOgrns.join(",");
 
-        const requisites = await sendRequest(`${API.orgReqs}?ogrn=${ogrns}`);
         const orgsMap = reqsToMap(requisites);
 
-        const analytics = await sendRequest(`${API.analytics}?ogrn=${ogrns}`);
         addInOrgsMap(orgsMap, analytics, "analytics");
 
-        const buh = await sendRequest(`${API.buhForms}?ogrn=${ogrns}`);
         addInOrgsMap(orgsMap, buh, "buhForms");
         render(orgsMap, orgOgrns);
     } catch (error) {
